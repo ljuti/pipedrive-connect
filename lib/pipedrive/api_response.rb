eb89ccbe9df923416618f554948fc741e63f53b3
Lauri Jutila <git@laurijutila.com>
2023-02-16 00:26:17 +0200

    Add methods for pagination use cases.

--- lib/pipedrive/api_response.rb
+++ lib/pipedrive/api_response.rb
@@ -10,6 +10,14 @@ module Pipedrive
       @response = response
     end
 
+    def has_more_items?
+      @response.fetch(:additional_data, {}).fetch(:pagination, {}).fetch(:more_items_in_collection, false)
+    end
+
+    def next_start
+      @response.fetch(:additional_data, {}).fetch(:pagination, {}).fetch(:next_start, 0)
+    end
+
     attr_reader :response
   end
 end
